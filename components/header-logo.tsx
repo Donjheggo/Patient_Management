e4773f332984fc77d2eb056b7392e4d160899691
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-11-18 18:13:58 +0800

    fix design

--- components/header-logo.tsx
+++ components/header-logo.tsx
@@ -1,11 +1,5 @@
-import { View } from "react-native";
 import { Text } from "./ui/text";
 
 export default function HeaderLogo() {
-  return (
-    <View className="flex flex-col">
-      <Text>Explore</Text>
-      <Text className="text-2xl font-bold text-primary">FPOP</Text>
-    </View>
-  );
+  return <Text className="text-2xl font-bold text-primary">FPOP</Text>;
 }
